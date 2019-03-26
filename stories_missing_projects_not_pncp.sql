70bf39ebcb584970ef8635c33a63860121459e7e
Steve Rhoads <steve.rhoads@radcorp.com>
2019-03-26 15:16:47 -0400

    Put the count on the top

--- stories_missing_projects_not_pncp.sql
+++ stories_missing_projects_not_pncp.sql
@@ -1,3 +1,19 @@
+
+SELECT
+  COUNT(*)
+FROM catalyst c
+  INNER JOIN reaction r
+    ON c.ident = r.catalyst_ident
+  INNER JOIN status_message sm
+    ON r.ident = sm.reaction_ident
+WHERE c.last_updated > '2019-02-14T00:00:00Z'
+  AND r.status = 3
+  AND c.source_system_ident <> 1
+  AND c.source_action = 'INSERT'
+  AND sm.message_text LIKE '%reason Project%'
+  AND r.target_object_type = 'HierarchicalRequirement'
+  ;
+
 SELECT
   c.source_object_id,
   c.xref_id,
@@ -18,18 +34,3 @@ WHERE c.last_updated > '2019-02-14T00:00:00Z'
   AND sm.message_text LIKE '%reason Project%'
   AND r.target_object_type = 'HierarchicalRequirement'
   ;
-
-  SELECT
-    COUNT(*)
-  FROM catalyst c
-    INNER JOIN reaction r
-      ON c.ident = r.catalyst_ident
-    INNER JOIN status_message sm
-      ON r.ident = sm.reaction_ident
-  WHERE c.last_updated > '2019-02-14T00:00:00Z'
-    AND r.status = 3
-    AND c.source_system_ident <> 1
-    AND c.source_action = 'INSERT'
-    AND sm.message_text LIKE '%reason Project%'
-    AND r.target_object_type = 'HierarchicalRequirement'
-    ;
